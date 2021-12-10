182c72c68a43dc16e221789757d016ef23015638
Sergio Lopez <slp@redhat.com>
2021-12-10 12:24:52 +0100

    krunfw: Don't panic when init dies
    In libkrun, the isolated process runs as PID 1. When it exits,
    trigger an orderly reboot instead of panic'ing.
    
    Also, silence reboot messages for the moment. Eventually we should
    play with the printk levels instead.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -64,6 +64,7 @@
 #include <linux/rcuwait.h>
 #include <linux/compat.h>
 #include <linux/io_uring.h>
+#include <linux/reboot.h>
 
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
@@ -791,8 +792,12 @@ void __noreturn do_exit(long code)
 		 * immediately to get a useable coredump.
 		 */
 		if (unlikely(is_global_init(tsk)))
+#ifdef VERBOSE_LIBKRUN
 			panic("Attempted to kill init! exitcode=0x%08x\n",
 				tsk->signal->group_exit_code ?: (int)code);
+#else
+			orderly_reboot();
+#endif
 
 #ifdef CONFIG_POSIX_TIMERS
 		hrtimer_cancel(&tsk->signal->real_timer);
--- kernel/reboot.c
+++ kernel/reboot.c
@@ -247,10 +247,12 @@ void kernel_restart(char *cmd)
 	kernel_restart_prepare(cmd);
 	migrate_to_reboot_cpu();
 	syscore_shutdown();
+#ifdef VERBOSE_LIBKRUN
 	if (!cmd)
 		pr_emerg("Restarting system\n");
 	else
 		pr_emerg("Restarting system with command '%s'\n", cmd);
+#endif
 	kmsg_dump(KMSG_DUMP_SHUTDOWN);
 	machine_restart(cmd);
 }
