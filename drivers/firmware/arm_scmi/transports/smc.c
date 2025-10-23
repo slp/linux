937675db2daada4b20adf05ad837d3888925f09e
Eric Chanudet <echanude@redhat.com>
2025-10-23 18:43:24 -0400

    firmware: arm_scmi: bump SMC max_msg_size
    Quell a bunch of EBUSY during boot up.
    
    scmi-perf-{uart,spi}* need larger messages to avoid the following spam:
      arm-scmi arm-scmi.10.auto: [Firmware Bug]: Failed to add opps_by_lvl at 1515870810 for scmi-perf-uart24 - ret:-16
    
    Signed-off-by: Eric Chanudet <echanude@redhat.com>

--- drivers/firmware/arm_scmi/transports/smc.c
+++ drivers/firmware/arm_scmi/transports/smc.c
@@ -277,7 +277,7 @@ static struct scmi_desc scmi_smc_desc = {
 	.ops = &scmi_smc_ops,
 	.max_rx_timeout_ms = 30,
 	.max_msg = 20,
-	.max_msg_size = 128,
+	.max_msg_size = 256,
 	/*
 	 * Setting .sync_cmds_atomic_replies to true for SMC assumes that,
 	 * once the SMC instruction has completed successfully, the issued
