1491b78029cdea8ca6486aa1b23ab7a9332647d0
Maxime Ripard <mripard@redhat.com>
2025-11-06 09:31:26 +0100

    firmware: qcom_scm: Introduce PAS context initialization helper function
    JIRA: https://issues.redhat.com/browse/VROOM-34030
    Upstream Status: Posted https://lore.kernel.org/r/20251104-kvm_rproc_v6-v6-4-7017b0adc24e@oss.qualcomm.com
    
    Author: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Date:   2025-11-04 13:05:44 +0530
    
        firmware: qcom_scm: Introduce PAS context initialization helper function
    
        When the Peripheral Authentication Service (PAS) method runs on a SoC
        where Linux operates at EL2 (i.e., without the Gunyah hypervisor), the
        reset sequences are handled by TrustZone. In such cases, Linux must
        perform additional steps before invoking PAS SMC calls, such as creating
        a SHM bridge. Therefore, PAS SMC calls require awareness and handling of
        these additional steps when Linux runs at EL2.
    
        To support this, there is a need for a data structure that can be
        initialized prior to invoking any SMC or MDT functions. This structure
        allows those functions to determine whether they are operating in the
        presence or absence of the Gunyah hypervisor and behave accordingly.
    
        Currently, remoteproc and non-remoteproc subsystems use different
        variants of the MDT loader helper API, primarily due to differences in
        metadata context handling. Remoteproc subsystems retain the metadata
        context until authentication and reset are completed, while
        non-remoteproc subsystems (e.g., video, graphics, IPA, etc.) do not
        retain the metadata context and can free it within the
        qcom_scm_pas_init() call by passing a NULL context parameter and due to
        these differences, it is not possible to extend metadata context
        handling to support remoteproc and non remoteproc subsystem use PAS
        operations, when Linux operates at EL2.
    
        Add PAS context data structure and initialization helper function.
    
        Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    
    Signed-off-by: Maxime Ripard <mripard@redhat.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -559,6 +559,42 @@ static void qcom_scm_set_download_mode(u32 dload_mode)
 }
 
 /**
+ * devm_qcom_scm_pas_context_init() - Initialize peripheral authentication service
+ *				      context for a given peripheral
+ *
+ * PAS context is device-resource managed, so the caller does not need
+ * to worry about freeing the context memory.
+ *
+ * @dev:	  PAS firmware device
+ * @pas_id:	  peripheral authentication service id
+ * @mem_phys:	  Subsystem reserve memory start address
+ * @mem_size:	  Subsystem reserve memory size
+ *
+ * Upon successful, returns the PAS context or ERR_PTR() of the error otherwise.
+ */
+void *devm_qcom_scm_pas_context_init(struct device *dev, u32 pas_id, phys_addr_t mem_phys,
+				     size_t mem_size)
+{
+	struct qcom_scm_pas_context *ctx;
+
+	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
+	if (!ctx)
+		return ERR_PTR(-ENOMEM);
+
+	ctx->dev = dev;
+	ctx->pas_id = pas_id;
+	ctx->mem_phys = mem_phys;
+	ctx->mem_size = mem_size;
+
+	ctx->metadata = devm_kzalloc(dev, sizeof(*ctx->metadata), GFP_KERNEL);
+	if (!ctx->metadata)
+		return ERR_PTR(-ENOMEM);
+
+	return ctx;
+}
+EXPORT_SYMBOL_GPL(devm_qcom_scm_pas_context_init);
+
+/**
  * qcom_scm_pas_init_image() - Initialize peripheral authentication service
  *			       state machine for a given peripheral, using the
  *			       metadata
