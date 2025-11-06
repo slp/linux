362c923427ccb0eb6633d74bb7d83722685b520f
Maxime Ripard <mripard@redhat.com>
2025-11-06 09:31:29 +0100

    soc: qcom: mdtloader: Remove qcom_mdt_pas_init() from exported symbols
    JIRA: https://issues.redhat.com/browse/VROOM-34030
    Upstream Status: Posted https://lore.kernel.org/r/20251104-kvm_rproc_v6-v6-7-7017b0adc24e@oss.qualcomm.com
    
    Author: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    Date:   2025-11-04 13:05:47 +0530
    
        soc: qcom: mdtloader: Remove qcom_mdt_pas_init() from exported symbols
    
        qcom_mdt_pas_init() was previously used only by the remoteproc driver
        (drivers/remoteproc/qcom_q6v5_pas.c). Since that driver has now
        transitioned to using PAS context-based qcom_mdt_pas_load() function,
        making qcom_mdt_pas_init() obsolete for external use.
    
        Removes qcom_mdt_pas_init() from the list of exported symbols and make
        it static to limit its scope to internal use within mdtloader.
    
        Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
    
    Signed-off-by: Maxime Ripard <mripard@redhat.com>

--- drivers/soc/qcom/mdt_loader.c
+++ drivers/soc/qcom/mdt_loader.c
@@ -238,9 +238,9 @@ EXPORT_SYMBOL_GPL(qcom_mdt_read_metadata);
  *
  * Returns 0 on success, negative errno otherwise.
  */
-int qcom_mdt_pas_init(struct device *dev, const struct firmware *fw,
-		      const char *fw_name, int pas_id, phys_addr_t mem_phys,
-		      struct qcom_scm_pas_context *ctx)
+static int __qcom_mdt_pas_init(struct device *dev, const struct firmware *fw,
+			       const char *fw_name, int pas_id, phys_addr_t mem_phys,
+			       struct qcom_scm_pas_context *ctx)
 {
 	const struct elf32_phdr *phdrs;
 	const struct elf32_phdr *phdr;
@@ -302,7 +302,6 @@ int qcom_mdt_pas_init(struct device *dev, const struct firmware *fw,
 out:
 	return ret;
 }
-EXPORT_SYMBOL_GPL(qcom_mdt_pas_init);
 
 static bool qcom_mdt_bins_are_split(const struct firmware *fw)
 {
@@ -456,7 +455,7 @@ int qcom_mdt_load(struct device *dev, const struct firmware *fw,
 {
 	int ret;
 
-	ret = qcom_mdt_pas_init(dev, fw, firmware, pas_id, mem_phys, NULL);
+	ret = __qcom_mdt_pas_init(dev, fw, firmware, pas_id, mem_phys, NULL);
 	if (ret)
 		return ret;
 
@@ -508,8 +507,8 @@ int qcom_mdt_pas_load(struct qcom_scm_pas_context *ctx, const struct firmware *f
 {
 	int ret;
 
-	ret = qcom_mdt_pas_init(ctx->dev, fw, firmware, ctx->pas_id, ctx->mem_phys,
-				ctx);
+	ret = __qcom_mdt_pas_init(ctx->dev, fw, firmware, ctx->pas_id, ctx->mem_phys,
+				  ctx);
 	if (ret)
 		return ret;
 
